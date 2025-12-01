91d73235a60eeea3aaf8cc3112a460d03b7e02f4
Claudio Jara <holasoyarturo123.com@gmail.com>
2025-12-01 12:14:55 -0500

    cambio de vesión de CURRENT_BUILD_CODE

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -18,7 +18,7 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import Toast from 'react-native-toast-message';
 import { getAppVersion } from "../services/historialService";
 
-const CURRENT_BUILD_CODE = "AppTecnicosV2.2.17";
+const CURRENT_BUILD_CODE = "AppTecnicosV2.2.18";
 const HomeScreen = () => {
   const navigation = useNavigation();
   const { signOut, userInfo } = useContext(AuthContext)
